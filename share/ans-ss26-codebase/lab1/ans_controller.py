b02567db2fdb5f013236163be318ec8b64c07742
Daniel <daniel.propp@mni.thm.de>
2026-05-11 14:51:29 +0200

    initialized controller

--- share/ans-ss26-codebase/lab1/ans_controller.py
+++ share/ans-ss26-codebase/lab1/ans_controller.py
@@ -109,7 +109,7 @@ class LearningSwitch(app_manager.RyuApp):
                 self.handle_ipv4(datapath, in_port, eth, ipv4_pkt, parser, ofproto, msg)
                 return
         elif dpid in [1,2]:
-            # TODO: add MAC learning and forwarding logic here
+            # TODO: add MAC learning and forwarding logic here PERSON 1 / ZAIN
             pass
     
     def handle_arp(self, datapath, in_port, eth, arp_pkt, parser, ofproto):
@@ -123,7 +123,7 @@ class LearningSwitch(app_manager.RyuApp):
         router_ip = self.port_to_own_ip.get(in_port)
 
         if target_ip == router_ip:
-            router_mac = self.port_to_own_mac(in_port)
+            router_mac = self.port_to_own_mac[in_port]
             
             reply_pkt = packet.Packet()
 
@@ -133,6 +133,7 @@ class LearningSwitch(app_manager.RyuApp):
                 src=router_mac,
                 ethertype=ether_types.ETH_TYPE_ARP
             )
+            reply_pkt.add_protocol(eth_reply)
             # reply ARP Header
             arp_reply = arp.arp(
                 hwtype = 1,
@@ -143,23 +144,68 @@ class LearningSwitch(app_manager.RyuApp):
                 src_mac=router_mac,
                 src_ip=router_ip,
                 dst_mac=arp_pkt.src_mac,
-                dst_ip=arp_pkt.scr_ip
+                dst_ip=arp_pkt.src_ip
             )
-        reply_pkt.add_protocol(arp_reply)
+            reply_pkt.add_protocol(arp_reply)
 
-        # transform the packet to a bytestream
-        reply_pkt.serialize()
+            # transform the packet to a bytestream
+            reply_pkt.serialize()
 
-        actions = [parser.OFPActionOutput(in_port)]
+            actions = [parser.OFPActionOutput(in_port)]
 
-        out = parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=ofproto.OFP_NO_BUFFER,
-            in_port=ofproto.OFPP_CONTROLLER,
-            actions=actions,
-            data=reply_pkt.data
-        )
-        datapath.send_msg(out)
+            out = parser.OFPPacketOut(
+                datapath=datapath,
+                buffer_id=ofproto.OFP_NO_BUFFER,
+                in_port=ofproto.OFPP_CONTROLLER,
+                actions=actions,
+                data=reply_pkt.data
+            )
+            datapath.send_msg(out)
 
     def handle_ipv4(self, datapath, in_port, eth, ipv4_pkt, parser, ofproto, msg): 
-        pass
+        dst_ip = ipv4_pkt.dst
+
+        # catch if its directet towards the router
+        if dst_ip in self.port_to_own_ip.values():
+            #TODO: ICMP-Reply handling PERSON 3 / AMIN
+            return
+
+        #determine out-port
+        out_port = None
+        for port, ip in self.port_to_own_ip.items():
+            if dst_ip.rsplit('.', 1)[0] == ip.rsplit('.', 1)[0]:
+                out_port = port
+                break
+        
+        if out_port is None:
+            return 
+
+        dst_mac = self.arp_cache.get(dst_ip)
+
+        if not dst_mac:
+            return
+        #TODO: Check traffix beteween ext PERSON 3 / AMIN
+        router_mac_out = self.port_to_own_mac[out_port]
+
+        match = parser.OFPMatch(eth_type=0x0800, ipv4_dst=dst_ip)
+
+        actions = [
+            # source mac -> outport
+            parser.OFPActionSetField(eth_src=router_mac_out),
+            # destmac -> receiver
+            parser.OFPActionSetField(eth_dst=dst_mac),
+            parser.OFPActionDecNwTtl(),
+            parser.OFPActionOutput(out_port)
+        ]
+
+        self.add_flow(datapath, 10, match, actions, msg.buffer_id)
+
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            out = parser.OFPPacketOut(
+                datapath=datapath,
+                buffer_id=msg.buffer_id,
+                in_port=in_port,
+                actions=actions,
+                data=msg.data
+            )
+        datapath.send_msg(out)
